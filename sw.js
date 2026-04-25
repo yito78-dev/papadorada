4a6940ce6a1d9904e1846e6da03fa1a0103b26f6
yito78-dev <yito78@gmail.com>
2026-04-25 18:58:30 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'papadorada-v1';
+const ARCHIVOS = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
